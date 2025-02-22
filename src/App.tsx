e392c48133b4f947f8c2ebc68fead0653710018c
percyzavala <percyzavala@gmail.com>
2025-02-21 22:17:42 -0500

    Se instalo y  agrego la configuracion del paquete react navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { View, Text, SafeAreaView } from 'react-native'
 const App = () => {
   return (
     
-    <SafeAreaView style={{flex:1}} >
-      <View >
-        <Text>App modificado</Text>
-      </View>
-    </SafeAreaView>
+    
+      <NavigationContainer >
+        <Text>App modificado2</Text>
+      </NavigationContainer>
+   
   )
 }
 export default App
